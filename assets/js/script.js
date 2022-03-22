1d49ddb4a2b6c48d14f72ef5fcf0cd4a18a50762
jrbeike <jrbeike@gmail.com>
2022-03-22 19:05:44 -0400

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,3 +2,11 @@ bulmaCarousel.attach('#carousel', {
     slidesToScroll: 1,
     slidesToShow: 1
 });
+
+
+//weather
+const apiKey = "29cddfeb1ce63551dd6d2d389e8b733a";
+var displayCurrentWeather = funtion(cityName) {
+     
+} 
+
