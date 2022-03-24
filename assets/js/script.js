83ea8b1ac97b0d44be23173c32a5999a992a1438
jrbeike <jrbeike@gmail.com>
2022-03-24 00:29:57 -0400

    update weather js

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,8 +5,40 @@ bulmaCarousel.attach('#carousel', {
 
 
 //weather
-const apiKey = "29cddfeb1ce63551dd6d2d389e8b733a";
-var displayCurrentWeather = funtion(cityName) {
-     
-} 
 
+const weather = {}
+
+weather.temperature = {
+    unit : "farenheight"
+}
+const key = "29cddfeb1ce63551dd6d2d389e8b733a";
+
+function setPosition(position){
+    let latitude = 41.0998;
+    let longitude = 80.6495;
+
+    getWeather(latitude,longitude);
+}
+
+function getWeather(latitude, longitude){
+    let api = `http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${key}`;
+
+    fetch (api)
+        .then (function(response){
+            let data =response.json();
+            return data;
+        })
+        .then(function(data){
+            weather.temperature.value=data.main.temp;
+            weather.description=data.weather[0].description;
+            weather.city=data.name
+        })
+        .then(function(){
+            displayWeather();
+        });
+}
+function displayWeather(){
+    temp.Element.innerHTML= `${weather.temperature.value}°<span>F</span>`;
+    descElement.innerHTML = weather.description;
+    locationElement.innerHTML = `${weather.city}, ${weather.country}`;
+}
